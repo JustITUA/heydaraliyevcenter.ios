843a4cab5bc8c653a484cbc8707895b35c62ffc2
Anton Rogachevskiy <tenkfm@gmail.com>
2014-06-11 11:18:19 +0300

    iOS Init

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '7.0'
+
+pod 'AFNetworking', '~> 1.3.3'
+pod 'Mantle' , '1.4.1'
+pod 'MBProgressHUD', '~> 0.8'
+pod 'IQKeyboardManager', '~>3.0.3'
---
+++ heydaraliyevcenter/hcViewController.m
@@ -0,0 +1,29 @@
+//
+//  hcViewController.m
+//  heydaraliyevcenter
+//
+//  Created by Anton Rogachevskiy on 6/11/14.
+//  Copyright (c) 2014 JustIT. All rights reserved.
+//
+
+#import "hcViewController.h"
+
+@interface hcViewController ()
+
+@end
+
+@implementation hcViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
